463ba15cb3a0ba05e1e92f99961946600138d862
Diego Arabel <diegoarabel45@gmail.com>
2023-05-29 15:39:39 -0300

    terminando cards

--- src/components/searchItem/SearchItem.jsx
+++ src/components/searchItem/SearchItem.jsx
@@ -1,8 +1,30 @@
 import "./searchItem.css"
+import Img1 from "../../img/habitacion-losalamos.jpg"
+
 
 const SearchItem = () => {
   return (
-    <div>SearchItem</div>
+    <div className="searchItem">
+      <img src={Img1} alt="losalamos" className="siImg"/>
+      <div className="siDesc">
+        <h1 className="siTitle">Suite Double</h1>
+        <span className="siDistance">200m from center</span>
+        <span className="siHotel">Hotel "Los Alamos"</span>
+        <span className="siStars">★★★★</span>
+        <span className="siCancel">Free Cancellation</span>
+      </div>
+      <div className="siDetails">
+        <div className="siRating">
+          <span>Excellent</span>
+          <button>9.0</button>
+        </div>
+        <div className="siDetailTexts">
+          <span className="siPrice">$2490</span>
+          <span className="siTaxOp">Includes taxes and fees</span>
+          <button className="siCheckButton">See availability</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
